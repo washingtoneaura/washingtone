fb0dd716d7477e04fda05ffcb7f48c5006937642
washingtoneaura <aurawashingtone@gmail.com>
2024-07-02 17:26:02 +0300

    first update - home page

--- js/main.js
+++ js/main.js
@@ -121,6 +121,35 @@
             }
         }
     });
+
+    // Project and Testimonial carousel
+    $(".project-carousel, .testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+			0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
     
 })(jQuery);
 
